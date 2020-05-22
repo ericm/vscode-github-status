46bee9a95ffe1a1b33e55d1e5513a0709e2996a5
Eric Moynihan <git@moynihan.io>
2020-05-22 18:19:03 +0100

    Added time

--- src/service/github.ts
+++ src/service/github.ts
@@ -1,11 +1,13 @@
 import * as vscode from "vscode";
 import { graphql as gitHubApi } from "@octokit/graphql";
 import { RequestParameters, graphql } from "@octokit/graphql/dist-types/types";
+import * as moment from "moment";
 
 interface IEnv {
   [key: string]: string | undefined;
 }
 
+const OFFSET = 10000;
 const changeUserStatusMutation = `
   mutation ($status: ChangeUserStatusInput!) {
     changeUserStatus(input: $status) {
@@ -21,7 +23,8 @@ const changeUserStatusMutation = `
 
 export default class {
   private __api: graphql;
-  private __expires: number = 30;
+  private __expires = 1;
+  private __start?: moment.Moment;
   public received = false;
   constructor(token?: string) {
     const config: RequestParameters = {};
@@ -36,11 +39,23 @@ export default class {
   }
 
   public async updateStatus(workspace: string) {
+    const time = moment(new Date());
+    let diff = "";
+    if (!this.__start) {
+      this.__start = time;
+    } else {
+      let diffN = time.diff(this.__start, "minutes");
+      diff = `(${diffN} minute(s))`;
+      if (diffN > 60) {
+        let diffNStr = time.diff(this.__start, "hours", true).toFixed(2);
+        diff = `(${diffNStr} hour(s))`;
+      }
+    }
     const status: UserStatus = {
       expiresAt: new Date(
-        new Date().getTime() + this.__expires * 60000
+        OFFSET + new Date().getTime() + this.__expires * 60000
       ).toISOString(),
-      message: `Working on ${workspace}`,
+      message: `Working on ${workspace} ${diff}`,
       emoji: ":zap:",
     };
     try {
