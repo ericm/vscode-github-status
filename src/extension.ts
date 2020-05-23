a585cf9082ddb22e81b96121b4b64e8ca03ac91e
Eric Moynihan <git@moynihan.io>
2020-05-23 19:58:35 +0100

    Added status bar icon to enable and disable

--- src/extension.ts
+++ src/extension.ts
@@ -14,60 +14,79 @@ export async function activate(context: vscode.ExtensionContext) {
   if (!folders || folders[0].uri.fsPath in config.get<string[]>("blacklist")!) {
     return;
   }
+  statusBarIcon.show();
   const token = config.get<string>("token");
   const gitHubService = new GitHubServce(token);
   if (gitHubService.received && vscode.workspace.name) {
     interval = await gitHubService.updateStatus(vscode.workspace.name);
   }
-  let disposable = vscode.commands.registerCommand(
-    "githubstatus.createToken",
-    async () => {
-      try {
-        const info = await vscode.window.showInformationMessage(
-          "This extension requires a GitHub token with ther [user] permission. To create a token, click the button",
-          { modal: true },
-          "Create Token"
-        );
-        if (info) {
-          // Create token
-          await vscode.env.openExternal(
-            vscode.Uri.parse("https://github.com/settings/tokens")
+  statusBarIcon.text = "GitHub Status Syncing";
+  statusBarIcon.command = "githubstatus.deactivate";
+  try {
+    let disposable = vscode.commands.registerCommand(
+      "githubstatus.createToken",
+      async () => {
+        try {
+          const info = await vscode.window.showInformationMessage(
+            "This extension requires a GitHub token with ther [user] permission. To create a token, click the button",
+            { modal: true },
+            "Create Token"
           );
-          vscode.commands.executeCommand("githubstatus.accessToken");
+          if (info) {
+            // Create token
+            await vscode.env.openExternal(
+              vscode.Uri.parse("https://github.com/settings/tokens")
+            );
+            vscode.commands.executeCommand("githubstatus.accessToken");
+          }
+          console.log(info);
+        } catch (err) {
+          console.error(err);
         }
-        console.log(info);
-      } catch (err) {
-        console.error(err);
       }
-    }
-  );
-  let accessToken = vscode.commands.registerCommand(
-    "githubstatus.accessToken",
-    async () => {
-      try {
-        const newToken = await vscode.window.showInputBox({
-          prompt: "Enter the github access token here",
-        });
-        if (!newToken) {
-          vscode.commands.executeCommand("githubstatus.accessToken");
-        } else {
-          await config.update("token", newToken, 1);
-          vscode.commands.executeCommand("githubstatus.restart");
+    );
+    let accessToken = vscode.commands.registerCommand(
+      "githubstatus.accessToken",
+      async () => {
+        try {
+          const newToken = await vscode.window.showInputBox({
+            prompt: "Enter the github access token here",
+          });
+          if (!newToken) {
+            vscode.commands.executeCommand("githubstatus.accessToken");
+          } else {
+            await config.update("token", newToken, 1);
+            vscode.commands.executeCommand("githubstatus.restart");
+          }
+        } catch (err) {
+          console.error(err);
         }
-      } catch (err) {
-        console.error(err);
       }
-    }
-  );
-  let restart = vscode.commands.registerCommand("githubstatus.restart", () => {
-    console.log("Restart");
-    config = vscode.workspace.getConfiguration("githubstatus");
-    activate(context);
-  });
-  context.subscriptions.push(disposable, accessToken, restart);
+    );
+    let restart = vscode.commands.registerCommand(
+      "githubstatus.restart",
+      () => {
+        console.log("Restart");
+        config = vscode.workspace.getConfiguration("githubstatus");
+        activate(context);
+      }
+    );
+    let deac = vscode.commands.registerCommand(
+      "githubstatus.deactivate",
+      () => {
+        console.log("Deactivating");
+        deactivate();
+      }
+    );
+    context.subscriptions.push(disposable, accessToken, restart, deac);
+  } catch {
+    console.log("Restarted");
+  }
 }
 
 export function deactivate() {
+  statusBarIcon.text = "GitHub Status Not Syncing";
+  statusBarIcon.command = "githubstatus.restart";
   if (interval) {
     clearInterval(interval);
   }
